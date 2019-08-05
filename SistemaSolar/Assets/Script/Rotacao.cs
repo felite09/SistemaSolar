18289799f0e9a0c89d1988b5809aed009ae95323
Felphz <felipejoliveira2@gmail.com>
2019-08-05 17:24:08 -0300

    Commit inicial

---
+++ SistemaSolar/Assets/Script/Rotacao.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotacao : MonoBehaviour
+{
+
+    [Range(0,80)]
+    [SerializeField] private float velocidade;
+  
+    void Update()
+    {
+        transform.Rotate(Vector3.up * velocidade * Time.deltaTime);
+    }
+}
